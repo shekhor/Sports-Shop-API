7d62011adf01f599b354c77710782241c7bd0c15
shekhor <shekhor.chanda333@gmail.com>
2024-03-09 14:10:27 -0600

    Get all products API with seeding data is implemented

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HPlusSport.API
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly ShopContext shopContext;
+
+        public ProductsController(ShopContext context)
+        {
+            shopContext = context;
+            shopContext.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return shopContext.Products.ToArray();
+        }
+    }
+}
+
